57af01b6504438836b30d64323323b255458cd49
renansantos <renansantosmendes@gmail.com>
2018-01-31 16:40:58 -0200

    - Erro de recarga da instância corrigido
    - implementação do algoritmo VNS
    - implementação do Simulated Annealing
    - método para realização de experimentos concluído
    - parâmetros setados
    - temperatura inicial calculada com base nas 10 soluções vizinhas dado uma solução inicial
    
    
    
    - NÃO ESTÁ PERCORRENDO TODAS AS SOLICITAÇÕES PARA INSERIR -> CORRIGIR
    - CORRIGIR VIZINHANÇA DE REALOCAÇÃO DE SOLICITAÇÕES -> a solicitação não é retirada da primeira rota e é inserida na segunda, assim o valor da função objetivo vai ser sempre maior, fazendo com que não haja nenhuma solução que seja melhorada com essa vizinhança
    - CORREÇÃO ANTERIOR ESTÁ SENDO FEITA + falta arrumar as rotas
    - implementação do algoritmo VND
    - falta implementar o algoritmo de pós-otimização
    - falta testar e achar onde esta o erro da repetição de solicitações em rotas distintas

---
+++ src/test/java/VRPDRTSD/InstanceExampleTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VRPDRTSD;
+
+import InstanceReader.Instance;
+import java.io.IOException;
+import jxl.read.biff.BiffException;
+import org.junit.Test;
+
+/**
+ *
+ * @author renansantos
+ */
+public class InstanceExampleTest {
+
+    @Test
+    public void instanceExample() throws IOException, BiffException {
+        int requestsNumber = 10;
+        String path = "/home/renansantos/Área de Trabalho/Excel Instances/";
+
+        Instance instance = new Instance();
+        instance.setNumberOfRequests(requestsNumber)
+                .setRequestTimeWindows(10)
+                .setInstanceSize("s")
+                .setNumberOfNodes(12)
+                .setNumberOfVehicles(250)
+                .setVehicleCapacity(4);
+
+        VRPDRTSD problem = new VRPDRTSD(instance, path);
+        problem.buildGreedySolution();
+    }
+}
